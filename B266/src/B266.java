cab4c9201b2f811b26f44a1f3100f9a967f0d5c3
Uğur Erdem <43253140+kugurerdem@users.noreply.github.com>
2018-10-20 00:39:35 +0300

    Add files via upload

--- B266/src/B266.java
+++ B266/src/B266.java
@@ -6,7 +6,8 @@ public class B266 {
 		// Input variables
 		int a;
 		int b;
-		String position;
+		String str;
+		char[] position;
 		
 		// Scanner
 		Scanner in = new Scanner(System.in);
@@ -14,41 +15,24 @@ public class B266 {
 		// Get the values from the user
 		a = in.nextInt();
 		b = in.nextInt();
-		position = in.next();
+		str = in.next();
+		
+		position = str.toCharArray();
 		
 		// Do the calculations
 		for(int i = 0; i < b; i++) {
-			position = NextPosition(position);
-		}
-		
-		// Output the result
-		System.out.println(position);
-	}
-	
-	
-	// Function that changes positions for just one time
-	public static String NextPosition(String position) {
-		int i = 0;
-		String pos = position;
-		
-		
-		while(i < pos.length() - 1 ) {
-			if( pos.charAt(i) == 'B' && pos.charAt(i+1) == 'G') {
-				pos = changeChar(pos, i, 'G');
-				pos = changeChar(pos, i+1, 'B');
-				i = i + 2;
-			} else {
-				i++;
+			while(i < position.length - 1 ) {
+				if( position[i] == 'B' && position[i+1] == 'G') {
+					position[i] = 'G';
+					position[i+1] = 'B';
+					i = i + 2;
+				} else {
+					i++;
+				}
 			}
 		}
 		
-		return pos;
-	}
-	
-	// Function that changes a character in a string at specific index
-	public static String changeChar(String str, int a, char x) {
-		char[] Chars = str.toCharArray();
-		Chars[a] = x;
-		return String.valueOf(Chars);
+		// Output the result
+		System.out.println( String.valueOf(position) );
 	}
 }
